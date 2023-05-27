9980ce9c801b3d763e1d046dd807f0af8adbd7d0
[Diegobrls] <[dsbriales.99@gmail.com]>
2023-05-27 14:22:26 +0200

    commit cambios estancias

--- js/funciones.js
+++ js/funciones.js
@@ -1,16 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtener el formulario
-    var form = document.getElementById('contactForm');
+    // Obtener el botón de enviar
+    var submitButton = document.getElementById('submitButton');
 
-    // Agregar un controlador de eventos para el evento de envío del formulario
-    form.addEventListener('submit', function (event) {
+    // Agregar un controlador de eventos para el evento de clic en el botón de enviar
+    submitButton.addEventListener('click', function (event) {
         // Detener el envío del formulario
         event.preventDefault();
 
+        // Obtener el formulario
+        var form = document.getElementById('contactForm');
+
         // Validar el formulario
         if (form.checkValidity()) {
-            // Si el formulario es válido, enviarlo
-            form.submit();
+            // Mostrar el popover
+            submitButton.setAttribute('data-bs-toggle', 'popover');
+            submitButton.setAttribute('data-bs-content', 'Mensaje enviado correctamente');
+            submitButton.setAttribute('data-bs-placement', 'top');
+            var popover = new bootstrap.Popover(submitButton);
+            popover.show();
+
+            // Borrar el contenido del formulario después de 3 segundos
+            setTimeout(function () {
+                form.reset();
+                form.classList.remove('was-validated');
+            });
         } else {
             // Si el formulario no es válido, mostrar los mensajes de error y resaltar los campos inválidos
             form.classList.add('was-validated');
